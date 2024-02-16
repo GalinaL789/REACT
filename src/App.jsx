fcc62cb7c6fd73b73c024df05516cbcec19dca66
Galina Lasko <galina_lasko@hotmail.com>
2024-02-16 18:12:11 +0100

    addProfileCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,36 @@
-import "./App.css";
-import Button from "./components/Button/Button";
+import './App.css';
+import ProfileCard from './components/ProfileCard';
+const users = [
+  {
+    src: "https://images.pexels.com/photos/792381/pexels-photo-792381.jpeg?auto=compress&cs=tinysrgb&w=600",
+    name: 'Ivan',
+    lastName: 'Ivanov',
+  },
+  {
+    src: 'https://th.bing.com/th/id/OIP.2ejOJtifQCVnTBQ2WcW59QHaE7?w=223&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7',
+    name: 'John',
+    lastName: 'Smith',
+  },
+  {
+    src: 'https://th.bing.com/th/id/OIP.nq6r6yTeNKIw5OHeU1zR3gHaEK?w=226&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7',
+    name: 'Bob',
+    lastName: 'Harvy',
+  },
+];
 
-// 1. Компоненты пишутся с заглавной буквы
-// 2. Компоненты должны возвращать JSX(DOM элементы - HTML теги)
 function App() {
-  const nameOfCard = "Animal";
-
-  const animal = {
-    name: "Tiger",
-    age: 15,
-    src: "https://files.worldwildlife.org/wwfcmsprod/images/Tiger_resting_Bandhavgarh_National_Park_India/hero_small/6aofsvaglm_Medium_WW226365.jpg",
-  };
-
   return (
-    <div className="App">
-      <p>React app lesson 17</p>
-      <div className="button-container">
-        <Button />
-        <Button />
-        <Button />
-      </div>
-      <div className="car-card">
-        <h3>{nameOfCard}</h3>
-        <img src={animal.src} alt="Tiger image" />
-        <p>Name: {animal.name}</p>
-        <p>Age: {animal.age}</p>
-      </div>
+    <div className="page_wrapper">
+      {users.map((user, i) => {
+        return (
+          <ProfileCard
+            key={i}
+            src={user.src}
+            name={user.name}
+            lastName={user.lastName}
+          />
+        );
+      })}
     </div>
   );
 }
